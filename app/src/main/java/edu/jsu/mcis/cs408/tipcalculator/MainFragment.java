f28bce97d7fce17770262b691dba92e2e2fbafff
Anthony Howell <anthonyamh@gmail.com>
2021-03-06 18:54:44 -0600

    Finished checkValid() function

--- app/src/main/java/edu/jsu/mcis/cs408/tipcalculator/MainFragment.java
+++ app/src/main/java/edu/jsu/mcis/cs408/tipcalculator/MainFragment.java
@@ -4,17 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
 
 import androidx.fragment.app.Fragment;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainFragment extends Fragment {
 
-    private TextInputLayout totalBill, tipPercentage, numberPeople, textOutput;
+    private TextInputEditText totalBill, tipPercentage, numberPeople, textOutput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,32 +24,35 @@ public class MainFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        totalBill = (TextInputLayout) view.findViewById(R.id.totalBill);
-        tipPercentage = (TextInputLayout) view.findViewById(R.id.tipPercentage);
-        numberPeople = (TextInputLayout) view.findViewById(R.id.numberPeople);
+        totalBill = (TextInputEditText) view.findViewById(R.id.totalBill);
+        tipPercentage = (TextInputEditText) view.findViewById(R.id.tipPercentage);
+        numberPeople = (TextInputEditText) view.findViewById(R.id.numberPeople);
         view.findViewById(R.id.buttonCalculate).setOnClickListener(this::onClick);
     }
 
     public void onClick(View v) {
+        //Check inputs for any errors
         checkValid(totalBill);
+        checkValid(tipPercentage);
+        checkValid(numberPeople);
     }
 
-    public boolean checkValid(TextInputLayout t){
+    public boolean checkValid(TextInputEditText t){
         //Check if the input is blank, not a number, or 0
-        String s = t.getEditText().toString();
+        String s = t.getText().toString();
 
-        if (s == ""){
-            Toast.makeText(getActivity(), "Values cannot be blank!", Toast.LENGTH_LONG).show();
+        if (s.trim().isEmpty() || s == null){
+            Toast.makeText(getActivity(), "Values cannot be blank!", Toast.LENGTH_SHORT).show();
             return false;
         }
 
-        if (s != null && s.matches("[-+]?\\d*\\.?\\d+") == false){
-            Toast.makeText(getActivity(), "Values must be numeric!", Toast.LENGTH_LONG).show();
+        if (s != null && !s.matches("[-+]?\\d*\\.?\\d+")){
+            Toast.makeText(getActivity(), "Values must be numeric!", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         if (Integer.parseInt(s) == 0){
-            Toast.makeText(getActivity(), "Values cannot be zero!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Values cannot be zero!", Toast.LENGTH_SHORT).show();
             return false;
         }
 
